d86139665ac2216cf538562ffe12216fb4e84fd9
a-japonicus <ajapon88@gmail.com>
2013-11-21 10:15:35 +0900

    変数名変更

--- server/application/modules/jsonrpc/controllers/IndexController.php
+++ server/application/modules/jsonrpc/controllers/IndexController.php
@@ -2,24 +2,27 @@
 
 class Jsonrpc_IndexController extends Zend_Controller_Action
 {
-
+    protected $_server = null;
+    
     public function init()
     {
+        // view自体は非表示
         $this->_helper->viewRenderer->setNoRender(true);
     
-        $this->server = new Zend_Json_Server();
-        $this->server->setClass('Jsonrpc_Service_Calculator','Calculator');
+        $this->_server = new Zend_Json_Server();
+        // クラス追加
+        $this->_server->setClass('Jsonrpc_Service_Calculator','Calculator');
     }
 
     public function indexAction()
     {
         if ('POST' != strtoupper(filter_input(INPUT_SERVER, 'REQUEST_METHOD'))) {
             // URLのエンドポイント及び使用するJSON-RPCのバージョンを示します:
-            $this->server->setTarget('/jsonrpc')
+            $this->_server->setTarget('/jsonrpc')
                    ->setEnvelope(Zend_Json_Server_Smd::ENV_JSONRPC_2);
 
             // SMDをつかみます
-            $smd = $this->server->getServiceMap();
+            $smd = $this->_server->getServiceMap();
 
             // クライアントにSMDを返します
             header('Content-Type: application/json');
@@ -27,7 +30,7 @@ class Jsonrpc_IndexController extends Zend_Controller_Action
             return;
         }
         
-        $this->server->handle();
+        $this->_server->handle();
     }
 }
 
--- server/application/modules/xmlrpc/controllers/IndexController.php
+++ server/application/modules/xmlrpc/controllers/IndexController.php
@@ -2,27 +2,27 @@
 
 class Xmlrpc_IndexController extends Zend_Controller_Action
 {
-    protected $server = null;
+    protected $_server = null;
 
     public function init()
     {
         // view自体は非表示
         $this->_helper->viewRenderer->setNoRender(true);
         
-        $this->server = new Zend_XmlRpc_Server();
+        $this->_server = new Zend_XmlRpc_Server();
         // クラス追加
-        $this->server->setClass('Xmlrpc_Service_Calculator', 'Calculator');
+        $this->_server->setClass('Xmlrpc_Service_Calculator', 'Calculator');
     }
 
     public function indexAction()
     {
         // POSTのみ許可
         if ('POST' != $_SERVER['REQUEST_METHOD']) {
-            echo $this->server->fault();
+            echo $this->_server->fault();
             return;
         }
         // 結果表示
-        echo $this->server->handle();
+        echo $this->_server->handle();
     }
 }
 
